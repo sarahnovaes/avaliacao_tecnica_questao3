99ace2dae360f27675595f2e2275da534437661b
sarahnovaes <118676911+sarahnovaes@users.noreply.github.com>
2022-11-20 12:22:15 -0300

    Add files via upload

---
+++ main3.js
@@ -0,0 +1,15 @@
+const pessoas = [
+    { name: "Fabiana Araújo", age: 33 },
+    { name: "Gabriel Gomes", age: 25 },
+    { name: "Fernando Henrique", age: 17 },
+    { name: "Ana Luiza", age: 2 },
+    { name: "Geralda do Nascimento", age: 93 },
+    { name: "Miguel Souza", age: 70 },
+    { name: "Antonio Miguel", age: 69 },
+  ];
+  
+var entrada = prompt("Digite o nome da pessoa:    (Ex.: Gabriel Gomes)");
+
+const result = pessoas.find(({name}) => name === entrada);
+
+console.log(result);
